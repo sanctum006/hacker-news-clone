9d26ae6bea67f295c2291569ec8aaa75e384dfd9
pushpendray007 <pushpendra@weekday.team>
2022-08-06 21:33:41 +0530

    fixed loader for post page

--- src/Components/Utilities/Loader.js
+++ src/Components/Utilities/Loader.js
@@ -1,12 +1,15 @@
 import { CircularProgress } from "@mui/material";
 import { FULFILLED, PENDING, REJECTED } from "../../constants/posts";
-import { customLoaderStyle } from "../../constants/styles";
+import {
+  customLoaderContainerStyle,
+  customLoaderStyle,
+} from "../../constants/styles";
 
 function Loader({ status, result, defaultText }) {
   switch (status) {
     case PENDING: {
       return (
-        <div style={{ width: "100%", textAlign: "center" }}>
+        <div style={customLoaderContainerStyle}>
           <CircularProgress style={customLoaderStyle} size={60} />
         </div>
       );
--- src/constants/styles.js
+++ src/constants/styles.js
@@ -2,6 +2,15 @@ export const customLoaderStyle = {
   color: "#61dafb",
 };
 
+export const customLoaderContainerStyle = {
+  width: "100%",
+  textAlign: "center",
+  height: "300px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 export const customLinkStyle = {
   textDecoration: "none",
   color: "aliceblue",
